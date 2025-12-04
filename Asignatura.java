c650385d06ebf25ad93c547f77014217272604f6
cokeIkigai <coke_vader@hotmail.com>
2025-12-04 15:58:11 +0100

    Create Asignatura class with method stubs
    Define the Asignatura class with properties and method stubs.

---
+++ Asignatura.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Asignatura {
+    private String nombre;
+    private List<Integer> notas;
+
+    // Crea el constructor:
+
+
+    // Método agregarNota(int n)
+    // - Solo añadir valores entre 0 y 10
+
+
+    // Método getNotas() de una asignatura concreta
+    // - Devolver la lista completa de notas
+
+
+    // Método promedio() de una asignatura
+    // - Si no hay notas -> devolver -1
+    // - Calcular el promedio manualmente
+
+
+    // Método notaMaxima() de dicha asignatura
+    // - Si no hay notas -> devolver -1
+    // - Calcular el máximo manualmente
+
+
+    // Método notaMinima() de dicha asignatura
+    // - Si no hay notas -> devolver -1
+    // - Calcular el mínimo manualmente
+}
+
